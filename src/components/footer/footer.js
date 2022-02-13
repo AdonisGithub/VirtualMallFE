fc9235080c506a7edaffe734fc6deb2f16918c63
adonis <goldendores9826@gmail.com>
2022-02-13 21:08:49 +0800

    favicon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     <div className=" bg-black">
         <div className="max-w-2xl h-[75px] lg:h-[120px] xxl:h-[157px] mx-auto text-white flex items-center justify-center py-3">
           <div className="text-center">
-            <p className="text-lg "> Developed by VR mall solutions</p>
+            <p className="text-lg "> Powered by Pro XR Solutions</p>
             <div className="text-lg mb-1 flex justify-center gap-2 max-w-full overflow-hidden">
               <div className="flex">
-                <img src={internet} className="w-[15px] h-[17px] m-auto"alt="internet"/>&nbsp;<span>vrmallsolutions.com</span>
+                <img src={internet} className="w-[15px] h-[17px] m-auto"alt="internet"/>&nbsp;<a href="https://proxrsolutions.com" className="hover:text-red-600 active:text-blue-600">proxrsolutions.com</a>
               </div>
               <div className="flex">
-                <img src={mail} className="w-[15px] h-[17px] m-auto" alt="mail"/>&nbsp;<span>vrmallsolutions.com</span>
+                <img src={mail} className="w-[15px] h-[17px] m-auto" alt="mail"/>&nbsp;<a href="https://proxrsolutions.com" className="hover:text-red-600 active:text-blue-600">proxrsolutions.com</a>
               </div>
             </div>
           </div>
